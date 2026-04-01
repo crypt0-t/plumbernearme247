72a370f7e2db9438b8629289bc2ac204a5538583
crypt0-t <tomrow1986@gmail.com>
2026-04-01 01:35:42 +0000

    Add GSC verification meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="O46cEC_PsCTgWShXZNa0m7Z9-asPYH-pUPXxcJ0uOJc" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
